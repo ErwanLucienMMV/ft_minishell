e27273bdf5a46963882f80985e598b4340e5195d
Aurélien Barthès <aurelienbarthes@MacBook-Pro-de-Aurelien.local>
2026-02-06 19:27:25 +0100

    oui

--- expand/expand_d_quote.c
+++ expand/expand_d_quote.c
@@ -3,44 +3,75 @@
 /*                                                        :::      ::::::::   */
 /*   expand_d_quote.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emaigne <emaigne@student.42.fr>            +#+  +:+       +#+        */
+/*   By: abarthes <abarthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 02:16:39 by emaigne           #+#    #+#             */
-/*   Updated: 2026/02/03 22:44:31 by emaigne          ###   ########.fr       */
+/*   Updated: 2026/02/06 19:23:52 by abarthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expand.h"
 
-int static	handle_env_var(t_parser *node, t_envpath *envpath,
-	char **new_str, int *indice)
+static int	get_env_key(t_parser *node, int *indice, char **key)
 {
-	char	*value;
-	char	*key;
-	char	*temp;
 	int		end;
+	int		start;
+	int		len;
 
-	end = indice[0] + 1;
+	start = indice[0] + 1;
+	end = start;
 	while (node->s[end] && ft_isalnum(node->s[end]))
 		end++;
-	key = ft_substr(node->s, indice[0] + 1, end - (indice[0] + 1));
-	if (!key)
+	len = end - start;
+	*key = ft_substr(node->s, start, len);
+	if (!*key)
 		return (1);
-	value = get_env_value_by_key(&envpath, key);
-	temp = malloc(ft_strlen(*new_str) + ft_strlen(value) + 1);
+	indice[0] = end;
+	return (0);
+}
+
+static int	is_env_var(t_parser *node, int i)
+{
+	return (node->s[i] == '$' && ft_isalnum(node->s[i + 1]));
+}
+
+static int	append_value(char **new_str, int *indice, char *value)
+{
+	char	*temp;
+	size_t	value_len;
+
+	value_len = 0;
+	if (value)
+		value_len = ft_strlen(value);
+	temp = malloc(indice[1] + value_len + 1);
 	if (!temp)
-		return (free(key), 1);
-	ft_strlcpy(temp, *new_str, indice[1] + 1);
+		return (1);
+	ft_memcpy(temp, *new_str, indice[1]);
+	temp[indice[1]] = '\0';
 	free(*new_str);
 	*new_str = temp;
-	indice[2] = 0;
 	while (value && value[indice[2]])
 		(*new_str)[indice[1]++] = value[indice[2]++];
-	indice[0] = end;
+	(*new_str)[indice[1]] = '\0';
+	return (0);
+}
+
+static int	handle_env_var(t_parser *node, t_envpath *envpath,
+	char **new_str, int *indice)
+{
+	char	*key;
+	char	*value;
+
+	if (get_env_key(node, indice, &key))
+		return (1);
+	value = get_env_value_by_key(&envpath, key);
+	indice[2] = 0;
+	if (append_value(new_str, indice, value))
+		return (free(key), 1);
 	return (free(key), 0);
 }
 
-void static	set_node_type(t_parser *node)
+static void	set_node_type(t_parser *node)
 {
 	if (node->prev && node->prev->type == CMD)
 		node->type = CMD_ARG;
@@ -48,25 +79,33 @@ void static	set_node_type(t_parser *node)
 		node->type = CMD;
 }
 
+static int	init_expand(t_parser *node, char **new_str, int *indice, int *len)
+{
+	*len = ft_strlen(node->s);
+	*new_str = malloc((*len - 1) * sizeof(char));
+	if (!*new_str)
+		return (1);
+	indice[0] = 1;
+	indice[1] = 0;
+	return (0);
+}
+
 int	expand_d_quote(t_parser *node, t_envpath *envpath)
 {
 	char	*new_str;
 	int		indice[3];
 	int		len;
-
-	len = ft_strlen(node->s);
-	new_str = malloc((len - 1) * sizeof(char));
-	if (!new_str)
+	if (init_expand(node, &new_str, indice, &len))
 		return (1);
-	indice[0] = 1;
-	indice[1] = 0;
 	while (indice[0] < len - 1)
 	{
-		if (node->s[indice[0]] == '$' && ft_isalnum(node->s[indice[0] + 1])
-			&& (handle_env_var(node, envpath, &new_str, indice)))
-			return (free(new_str), 1);
-		if (!(node->s[indice[0]] == '$' && ft_isalnum(node->s[indice[0] + 1])))
-			new_str[indice[1]++] = node->s[indice[0]++];
+		if (is_env_var(node, indice[0]))
+		{
+			if (handle_env_var(node, envpath, &new_str, indice))
+				return (free(new_str), 1);
+			continue;
+		}
+		new_str[indice[1]++] = node->s[indice[0]++];
 	}
 	new_str[indice[1]] = '\0';
 	free(node->s);
