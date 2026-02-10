2bb78f34d68380be41fe5055969cba86bc5faa8f
Erwan Maigne <emaigne@f2r2s17.clusters.42paris.fr>
2026-02-10 09:09:14 +0100

    protected the split in create_envpath_list

--- envpath/envpath.c
+++ envpath/envpath.c
@@ -6,7 +6,7 @@
 /*   By: emaigne <emaigne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 18:31:45 by abarthes          #+#    #+#             */
-/*   Updated: 2026/02/10 08:59:37 by emaigne          ###   ########.fr       */
+/*   Updated: 2026/02/10 09:08:23 by emaigne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,8 @@ int	create_envpath_list(t_envpath **envpath, char **envp)
 		if (ft_strchr(envp[i], '='))
 		{
 			array = ft_split(envp[i], '=');
+			if (!array)
+				return (0);
 			if (new_envpath(envpath, array[0], array[1]) == 0)
 				return (0);
 			if (array)
