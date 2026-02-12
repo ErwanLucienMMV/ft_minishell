55923030967357c1437ac057928c14b843bda3de
Erwan Maigne <emaigne@f2r2s19.clusters.42paris.fr>
2026-02-12 09:29:48 +0100

    j'ai juste rep la norme un peu

--- execve/execve.h
+++ execve/execve.h
@@ -6,7 +6,7 @@
 /*   By: emaigne <emaigne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 15:43:59 by abarthes          #+#    #+#             */
-/*   Updated: 2026/02/11 18:11:46 by emaigne          ###   ########.fr       */
+/*   Updated: 2026/02/12 09:29:30 by emaigne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,31 +33,37 @@ typedef struct s_commands
 }	t_commands;
 
 //			---freeing functions---		//
-void	clearmatrix(char **tab);
-void	free_t_parser(t_parser *parser);
-void	free_t_command(t_commands *tofree);
+void			clearmatrix(char **tab);
+void			free_t_parser(t_parser *parser);
+void			free_t_command(t_commands *tofree);
 
 //				---Utils---				//
-void	do_command_piped(t_program *program, t_commands *cmd, char *path, char **envp);
-void	handle_the_child(int pipe_fd[2], t_program *program, t_commands *cmd);
-void	do_command(t_program *program, t_parser *cmd, char *path, char **envp);
+
+void			do_command_piped(t_program *program,
+					t_commands *cmd, char *path, char **envp);
+void			handle_the_child(int pipe_fd[2],
+					t_program *program, t_commands *cmd);
+void			do_command(t_program *program,
+					t_parser *cmd, char *path, char **envp);
 
 //				---Children---			//
-void	handle_middle_child(t_program *program,
-			t_commands *cmd, int pipe_fd[2]);
+void			handle_middle_child(t_program *program,
+					t_commands *cmd, int pipe_fd[2]);
 
 //				---Checks---			//
-int		there_is_at_least_one_pipe(t_parser *lineread);
-int		count_cmd_args(t_parser *cmd);
-int		is_a_buildin(char *cmd);
-t_parser	*get_last_input_node(t_parser *parsed, t_lexer *input_type);
+int				there_is_at_least_one_pipe(t_parser *lineread);
+int				count_cmd_args(t_parser *cmd);
+int				is_a_buildin(char *cmd);
+t_parser		*get_last_input_node(t_parser *parsed, t_lexer *input_type);
 
-char	*find_command(char *command, char *pathline);
-int		execve_handler(t_program *program);
-void	do_command(t_program *program, t_parser *cmd, char *path, char **envp);
-int		execve_with_pipe(t_program *program);
-void	parse_commands_with_pipe(t_commands **commands, t_parser *parsed);
-int		make_redirection(t_parser *parsed);
+char			*find_command(char *command, char *pathline);
+int				execve_handler(t_program *program);
+void			do_command(t_program *program, t_parser *cmd,
+					char *path, char **envp);
+int				execve_with_pipe(t_program *program);
+void			parse_commands_with_pipe(t_commands **commands,
+					t_parser *parsed);
+int				make_redirection(t_parser *parsed);
 
 
 #endif
