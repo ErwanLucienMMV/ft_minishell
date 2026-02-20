0b5fb22d2c98045f6dd086c5754d859bea9ab98a
Erwan Maigne <emaigne@f6r6s11.clusters.42paris.fr>
2026-02-20 12:26:52 +0100

    why????

--- execve/free_structures.c
+++ execve/free_structures.c
@@ -6,7 +6,7 @@
 /*   By: emaigne <emaigne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 06:15:20 by emaigne           #+#    #+#             */
-/*   Updated: 2026/02/20 08:13:41 by emaigne          ###   ########.fr       */
+/*   Updated: 2026/02/20 12:25:02 by emaigne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,16 @@ void	free_t_program(t_program *program)
 	{
 		free_parsers(*(program->parsed));
 		free(program->parsed);
+		program->parsed = NULL;
 	}
 	if (program->envpath)
 	{
 		free_envpath(*(program->envpath));
 		free(program->envpath);
+		program->envpath = NULL;
 	}
 	free(program);
+	program = NULL;
 }
 
 void	free_t_parser(t_parser *parser)
@@ -53,13 +56,22 @@ void	free_t_parser(t_parser *parser)
 	if (parser->next)
 		next = parser->next;
 	if (parser->s)
+	{
 		free(parser->s);
+		parser->s = NULL;
+	}
 	free(parser);
+	parser = NULL;
 	free_t_parser(next);
 }
 
 void	free_t_cmd_prgrm_exit(t_commands *cmd, t_program *program)
 {
+	if (IS_DEBUG)
+	{
+		ft_printf_fd(2, "an issue while setting %s",
+			"the pipes append and we exited\n");
+	}
 	free_t_command(cmd);
 	free_t_program(program);
 	exit(1);
