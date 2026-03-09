06563775882928f147f772d4fbdbc4ff55c4e195
Erwan Maigne <emaigne@f4r5s11.clusters.42paris.fr>
2026-03-09 16:01:21 +0100

    less segfault

--- libft/get_next_line_utils.c
+++ libft/get_next_line_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abarthes <abarthes@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emaigne <emaigne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 18:48:20 by abarthes          #+#    #+#             */
-/*   Updated: 2025/12/28 14:42:46 by abarthes         ###   ########.fr       */
+/*   Updated: 2026/03/09 15:58:55 by emaigne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strjoin_gnl(char *s1, char *s2)
 	else
 		strjoined = malloc((1 + ft_strlen(s1) + ft_strlen(s2)) * sizeof(char));
 	if (!strjoined)
-		return (0);
+		return (NULL);
 	i = 0;
 	j = 0;
 	while (s1 && s1[i])
